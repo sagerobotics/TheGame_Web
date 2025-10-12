7c369612859c40ece6be2bd0cd79934c33ee28ff
Alex McClarty <19677303+sagerobotics@users.noreply.github.com>
2025-10-11 21:29:29 -0500

    Update game.js

--- game.js
+++ game.js
@@ -89,7 +89,7 @@ const flag = {
 
 let cameraX = 0;
 const girlImg = new Image();
-girlImg.src = "girl.png";
+girlImg.src = "newgirl2.png";
 
 girlImg.onload = () => {
     console.log("Sprite sheet loaded", girlImg.width, girlImg.height);
@@ -407,5 +407,6 @@ function drawBeamEffect(player) {
 
 function loop() { update(); draw(); requestAnimationFrame(loop); }
 loop();
+
 
 
